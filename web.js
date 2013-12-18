5159f8b7c581ddcdd5e853fbad246a0c59815975
jeremiej <jeremie.cpt@gmail.com>
2013-12-18 10:05:17 +0100

    init

--- web.js
+++ web.js
@@ -1,17 +1,50 @@
 /**
  * Created by techlabs on 18/12/2013.
  */
-var express = require("express");
-var logfmt = require("logfmt");
-var app = express();
+var static = require('node-static');
+var http = require('http');
+var file = new(static.Server)();
+var app = http.createServer(function (req, res) {
+    file.serve(req, res);
+}).listen(2013);
 
-app.use(logfmt.requestLogger());
+var io = require('socket.io').listen(app);
+io.sockets.on('connection', function (socket){
 
-app.get('/', function(req, res) {
-    res.send('Hello World!');
+    function log(){
+        var array = [">>> Message from server: "];
+        for (var i = 0; i < arguments.length; i++) {
+            array.push(arguments[i]);
+        }
+        socket.emit('log', array);
+    }
+
+    socket.on('message', function (message) {
+        log('Got message: ', message);
+        // For a real app, should be room only (not broadcast)
+        socket.broadcast.emit('message', message);
+    });
+
+    socket.on('create or join', function (room) {
+        var numClients = io.sockets.clients(room).length;
+
+        log('Room ' + room + ' has ' + numClients + ' client(s)');
+        log('Request to create or join room', room);
+
+        if (numClients == 0){
+            socket.join(room);
+            socket.emit('created', room);
+        } else if (numClients == 1) {
+            io.sockets.in(room).emit('join', room);
+            socket.join(room);
+            socket.emit('joined', room);
+        } else { // max two clients
+            socket.emit('full', room);
+        }
+        socket.emit('emit(): client ' + socket.id + ' joined room ' + room);
+        socket.broadcast.emit('broadcast(): client ' + socket.id + ' joined room ' + room);
+
+    });
+
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-    console.log("Listening on " + port);
-});
